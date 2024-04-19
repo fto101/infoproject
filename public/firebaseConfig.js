8ac70983e76de3d84c08eff4be127bf54b622b0a
Nathan Barnes <nb2216515@gmail.com>
2024-04-19 20:56:39 +0000

    hi

--- public/firebaseConfig.js
+++ public/firebaseConfig.js
@@ -1,16 +1,15 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBPi6x_C6vusbUq-7E1_FQJfDJfNB_aghk",
-  authDomain: "infopro-ebb46.firebaseapp.com",
-  projectId: "infopro-ebb46",
-  storageBucket: "infopro-ebb46.appspot.com",
-  messagingSenderId: "115588785581",
-  appId: "1:115588785581:web:4732328b96589a141b6d24"
+var firebase = require("firebase/app");
+require("firebase/auth");
+require("firebase/firestore");
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDnHxWOJNks8qs5pw40EUcHqTjDRuW_3No",
+  authDomain: "info-32cec.firebaseapp.com",
+  projectId: "info-32cec",
+  storageBucket: "info-32cec.appspot.com",
+  messagingSenderId: "712292470007",
+  appId: "1:712292470007:web:121be4bf4807a2923b57f2"
 };
 
 // Initialize Firebase
